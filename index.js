7af235966311f4666d15e190b9c77dfc51d8b99a
HazemDesouky00 <HD2400936@tkh.edu.eg>
2025-12-13 16:32:58 +0300

    fixed the cors line to see the frontend port because it didnt see it when i tried the http://localhost but worked with the 127.0.0.1:5500 since thats the port the frontend was live on when i opened the file with live server

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-    origin: "http://localhost:5500", // port number of front end 
+    origin: ["http://localhost:5500","http://127.0.0.1:5500"], // port number of front end 
     credentials: true  
 }))
 
